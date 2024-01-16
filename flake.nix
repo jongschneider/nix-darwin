e73065e1e1be0efcded8bb8dc6d3e503e3e854bb
Jonathan Schneider <jonathan.schneider@thetalake.com>
2024-01-15 23:28:35 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,112 @@
+{
+  description = "Darwin configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager}:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      services.karabiner-elements.enable = true;
+
+      environment = {
+        shells = with pkgs; [ bash zsh ];
+        loginShell = pkgs.zsh;
+        systemPackages = with pkgs; [ 
+          coreutils
+        ];
+        systemPath = [ "/opt/homebrew/bin" ];
+        pathsToLink = [ "/Applications" ];
+      };
+      # nix.package = pkgs.nix;
+
+      fonts.fontDir.enable = true; # DANGER
+      fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }) ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # MacOS Configuration
+      system = {
+
+        defaults = {
+          finder.AppleShowAllExtensions = true;
+          finder._FXShowPosixPathInTitle = true;
+          finder.ShowPathbar = true;
+          NSGlobalDomain.InitialKeyRepeat = 14;
+          NSGlobalDomain.KeyRepeat = 1;
+          dock.autohide = true;
+        };
+        
+        # Used for backwards compatibility, please read the changelog before changing.
+        stateVersion = 4;
+
+        keyboard.enableKeyMapping = true;
+        keyboard.remapCapsLockToEscape = true;
+      };
+
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+
+      # Add ability to used TouchID for sudo authentication
+      security.pam.enableSudoTouchIdAuth = true;
+
+      users.users.jschneider = {
+          name = "jschneider";
+          home = "/Users/jschneider";
+      };
+
+      # Homebrew stuff
+      homebrew = {
+        enable = true;
+        caskArgs.no_quarantine = true;
+        global.brewfile = true;
+        casks = [ 
+          "rectangle"
+          "shottr"
+        ];
+        taps = [];
+        brews = [ "trash" ];
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Jonathans-MacBook-Pro
+    darwinConfigurations."Jonathans-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration
+        home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jschneider = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."Jonathans-MacBook-Pro".pkgs;
+  };
+}
---
+++ home.nix
@@ -0,0 +1,144 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "jschneider";
+  home.homeDirectory = "/Users/jschneider";
+
+    # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # Basics
+    htop
+    coreutils
+    curl
+    wget
+    tree
+    procs
+    ripgrep
+    ffmpeg
+
+    # Dev
+    gh
+    fh
+    lf
+    jq
+    jqp
+    go
+    golangci-lint
+    nodejs_21
+    git-open
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  
+  programs.bat.enable = true;
+  programs.bat.config.theme = "TwoDark";
+  programs.direnv.enable = true;
+  programs.direnv.enableZshIntegration = true;
+  programs.direnv.nix-direnv.enable = true;
+  programs.fzf.enable = true;
+  programs.fzf.enableZshIntegration = true;
+  programs.eza.enable = true;
+  programs.eza.enableAliases = true;
+  programs.git.enable = true;
+  programs.git.aliases = {
+      a = "add";
+      c = "commit";
+      ca = "commit --amend";
+      can = "commit --amend --no-edit";
+      cl = "clone";
+      cm = "commit -m";
+      co = "checkout";
+      cp = "cherry-pick";
+      cpx = "cherry-pick -x";
+      d = "diff";
+      f = "fetch";
+      fo = "fetch origin";
+      fu = "fetch upstream";
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      pl = "pull";
+      pr = "pull -r";
+      ps = "push";
+      psf = "push -f";
+      rb = "rebase";
+      rbi = "rebase -i";
+      r = "remote";
+      ra = "remote add";
+      rr = "remote rm";
+      rv = "remote -v";
+      rs = "remote show";
+      st = "status";
+  };
+  programs.git.extraConfig = {
+    core = {
+       excludesfile = "/Users/jschneider/.gitignore";
+    };
+    url = {
+        "git@bitbucket.org:" = {
+        insteadOf = "https://bitbucket.org/";
+      };
+    };
+    pull = {
+        rebase=true;
+    };
+  };
+  programs.git.userName = "Jonathan Schneider";
+  programs.git.userEmail = "jonathan.schneider@thetalake.com";
+  programs.git.difftastic.enable = true;
+  programs.git.difftastic.display = "side-by-side-show-both";
+  programs.z-lua.enable = true;
+  programs.zsh.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.autocd = true;
+  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+  programs.zsh.shellAliases = {
+    # ls = "ls --color=auto -F";
+    nixswitch = "darwin-rebuild switch --flake ~/.config/nix-darwin/";
+    nixup = "pushd ~/.config/nix-darwin; nix flake update; nixswitch; popd";
+
+    # Work
+    runingester = "export INGESTER_ENV=.env && goi && ingester > tmp.txt";
+    runintegrator = "export INTEGRATOR_ENV=.env && goi && integrator  > tmp.txt";
+    runemailpreprocessor = "ENV_FILE=preprocessor.env.localdev go run ./cmd/email_preprocessor/...";
+    telcon = "tsh login --proxy=thetalake.teleport.sh --auth=google";
+    colcon = "echo 'creating an ssh tunnel to dev collector via teleport...\ntsh ssh -N -L 27777:localhost.thetalake.com:13579 ubuntu@ingester1.dev1.thetalake.com' && tsh ssh -N -L 27777:localhost.thetalake.com:13579 ubuntu@ingester1.dev1.thetalake.com";
+    goi = "go install ./... && go vet ./...";
+    c = "code .";
+    
+    # More git aliases
+    g = "git";
+    gs = "git status";
+    gd = "git diff";
+    gdt = "git difftool";
+    gdlist = "git diff --name-status";
+    gcm = "git commit -m";
+    gco = "git checkout";
+    gcob = "git checkout -b";
+    gcom = "git checkout master && git pull";
+    gignore = "c .git/info/exclude";
+  };
+  programs.starship.enable = true;
+  programs.starship.enableZshIntegration = true;
+
+  programs.alacritty = {
+    enable = true;
+    settings.font.normal.family = "MesloLGS Nerd Font Mono";
+    settings.font.size = 16;
+  };
+
+  # home.file.".zshrc".file = ./.zshrc
+}
