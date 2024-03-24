1d544990acc62443f207d62d08a12f86995cfcad
Jonathan Schneider <jonathan.schneider@thetalake.com>
2024-03-24 15:30:38 -0400

    wilder floating in center of screen

--- home/nvim/lua/plugins/wilder.lua
+++ home/nvim/lua/plugins/wilder.lua
@@ -38,24 +38,44 @@ return {
 				),
 			})
 
-			wilder.set_option(
-				"renderer",
-				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-					highlighter = wilder.basic_highlighter(),
+			-- wilder.set_option(
+			-- 	"renderer",
+			-- 	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+			-- 		highlighter = wilder.basic_highlighter(),
+			-- 		highlights = {
+			-- 			default = text_highlight,
+			-- 			border = mauve_highlight,
+			-- 			accent = mauve_highlight,
+			-- 		},
+			-- 		pumblend = 5,
+			-- 		min_width = "100%",
+			-- 		min_height = "25%",
+			-- 		max_height = "25%",
+			-- 		border = "rounded",
+			-- 		left = { " ", wilder.popupmenu_devicons() },
+			-- 		right = { " ", wilder.popupmenu_scrollbar() },
+			-- 	}))
+			-- )
+            wilder.set_option('renderer', wilder.popupmenu_renderer(
+                wilder.popupmenu_palette_theme({
+                    highlighter = wilder.basic_highlighter(),
 					highlights = {
 						default = text_highlight,
 						border = mauve_highlight,
 						accent = mauve_highlight,
 					},
-					pumblend = 5,
-					min_width = "100%",
-					min_height = "25%",
-					max_height = "25%",
-					border = "rounded",
-					left = { " ", wilder.popupmenu_devicons() },
-					right = { " ", wilder.popupmenu_scrollbar() },
-				}))
-			)
+                    -- 'single', 'double', 'rounded' or 'solid'
+                    -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+                    border = 'rounded',
+                    max_height = '40%',      -- max height of the palette
+                    min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+                    prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+                    reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+                    left = { " ", wilder.popupmenu_devicons() },
+                    right = { " ", wilder.popupmenu_scrollbar() },
+					-- pumblend = 5,
+                })
+              ))
 		end,
 	},
 }
