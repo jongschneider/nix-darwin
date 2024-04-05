fe9db9435b76a07a848596782a4f1040405814e1
Jonathan Schneider <jonathan.schneider@thetalake.com>
2024-04-05 19:30:49 -0400

    wip: adds all keymaps i need

--- home/nvim/lua/core/keymaps.lua
+++ home/nvim/lua/core/keymaps.lua
@@ -1,6 +1,13 @@
 vim.g.mapleader = " "
 
+local nnoremap = require("core.keymap_utils").nnoremap
+local vnoremap = require("core.keymap_utils").vnoremap
+local inoremap = require("core.keymap_utils").inoremap
+local tnoremap = require("core.keymap_utils").tnoremap
+local xnoremap = require("core.keymap_utils").xnoremap
 local keymap = vim.keymap -- for conciseness
+
+local TERM = os.getenv("TERM")
 
 keymap.set("i", "jf", "<ESC>", { desc = "Exit insert mode with jk" })
 
@@ -9,6 +16,56 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- Normal --
+-- Disable Space bar since it'll be used as the leader key
+nnoremap("<space>", "<nop>")
+
+-- vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+-- vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+-- vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+-- vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Window +  better kitty navigation
+nnoremap("<C-j>", function()
+	if vim.fn.exists(":KittyNavigateDown") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateDown()
+	elseif vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
+		vim.cmd.NvimTmuxNavigateDown()
+	else
+		vim.cmd.wincmd("j")
+	end
+end)
+
+nnoremap("<C-k>", function()
+	if vim.fn.exists(":KittyNavigateUp") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateUp()
+	elseif vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
+		vim.cmd.NvimTmuxNavigateUp()
+	else
+		vim.cmd.wincmd("k")
+	end
+end)
+
+nnoremap("<C-l>", function()
+	if vim.fn.exists(":KittyNavigateRight") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateRight()
+	elseif vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
+		vim.cmd.NvimTmuxNavigateRight()
+	else
+		vim.cmd.wincmd("l")
+	end
+end)
+
+nnoremap("<C-h>", function()
+	if vim.fn.exists(":KittyNavigateLeft") ~= 0 and TERM == "xterm-kitty" then
+		vim.cmd.KittyNavigateLeft()
+	elseif vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
+		vim.cmd.NvimTmuxNavigateLeft()
+	else
+		vim.cmd.wincmd("h")
+	end
+end)
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
@@ -44,5 +101,86 @@ keymap.set("n", "<leader>3", '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', {
 keymap.set("n", "<leader>4", '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', { desc = "Quickly jump to harpooned file 4" })
 
 keymap.set("n", "<leader>5", '<cmd>lua require("harpoon.ui").nav_file(5)<CR>', { desc = "Quickly jump to harpooned file 5" })
+
+-- Save with leader key
+nnoremap("<leader>w", "<cmd>w<cr>", { silent = false })
+
+-- Press 'H', 'L' to jump to start/end of a line (first/last char)
+nnoremap("L", "$")
+nnoremap("H", "^")
+
+-- Press 'H', 'L' to jump to start/end of a line (first/last char)
+vnoremap("L", "$<left>")
+vnoremap("H", "^")
+
+-- Press 'U' for redo
+nnoremap("U", "<C-r>")
+
+-- Center buffer while navigating
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("{", "{zz")
+nnoremap("}", "}zz")
+nnoremap("N", "Nzz")
+nnoremap("n", "nzz")
+nnoremap("G", "Gzz")
+nnoremap("gg", "ggzz")
+nnoremap("<C-i>", "<C-i>zz")
+nnoremap("<C-o>", "<C-o>zz")
+nnoremap("%", "%zz")
+nnoremap("*", "*zz")
+nnoremap("#", "#zz")
+
+-- Diagnostics
+
+-- Goto next diagnostic of any severity
+nnoremap("]d", function()
+	vim.diagnostic.goto_next({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous diagnostic of any severity
+nnoremap("[d", function()
+	vim.diagnostic.goto_prev({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next error diagnostic
+nnoremap("]e", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous error diagnostic
+nnoremap("[e", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next warning diagnostic
+nnoremap("]w", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous warning diagnostic
+nnoremap("[w", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+
+-- Press leader rw to rotate open windows
+nnoremap("<leader>rw", ":RotateWindows<cr>", { desc = "[R]otate [W]indows" })
+
+-- Press gx to open the link under the cursor
+nnoremap("gx", ":sil !open <cWORD><cr>", { silent = true })
+
+-- Paste without losing the contents of the register
+xnoremap("<leader>p", '"_dP')
+
+-- Move line up/down
+vnoremap("J", ":m '>+1<CR>gv=gv")
+vnoremap("K", ":m '<-2<CR>gv=gv")
 
 -- TODO: Telescope lsp_document_symbols ignore_symbols=variable
