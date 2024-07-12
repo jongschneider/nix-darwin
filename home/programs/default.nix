60ec7b7f918b698c0b48ed0c1b22907e1bdd7448
Jonathan Schneider <jonathan.schneider@thetalake.com>
2024-07-12 17:21:44 -0400

    adds atuin

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -58,6 +58,21 @@ in
         enableZshIntegration = true;
       };
 
+      atuin = {
+        enable = true;
+        enableZshIntegration = true;
+        settings = {
+          auto_sync = true;
+          sync_frequency = "5m";
+          sync_address = "https://api.atuin.sh";
+          # key_path = "${config.home.homeDirectory}/secrets/ATUIN_KEY";
+          ctrl_n_shortcuts = true;
+          enter_accept = true;
+          filter_mode = "session";
+        };
+        flags = ["--disable-up-arrow"];
+      };
+
       lazygit = {
         enable = true;
         catppuccin = {
