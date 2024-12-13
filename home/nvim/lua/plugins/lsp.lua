ff7608f9c0c3943162ee604fb5039dfbebe7015b
Jonathan Schneider <jonathan.schneider@thetalake.com>
2024-12-13 16:25:23 -0500

    fixes goimports autofmt in neovim

--- home/nvim/lua/plugins/lsp.lua
+++ home/nvim/lua/plugins/lsp.lua
@@ -41,6 +41,15 @@ return {
 				},
 				trouble = true,
 			})
+
+			local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*.go",
+				callback = function()
+					require("go.format").goimports()
+				end,
+				group = format_sync_grp,
+			})
 		end,
 		event = { "CmdlineEnter" },
 		ft = { "go", "gomod" },
