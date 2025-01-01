bc3a801299d212232c53eb7c0b69b6d5926a100f
Jonathan Schneider <jonathan.schneider@thetalake.com>
2025-01-01 16:14:55 -0500

    karabiner escape to caps_lock

--- home/default.nix
+++ home/default.nix
@@ -30,9 +30,8 @@ in {
     wezterm = {
       source = ./wezterm;
     };
-
     "karabiner/assets/complex_modifications/nix.json".text = builtins.toJSON {
-      title = "CapsLock modifier - Nix managed";
+      title = "CapsLock and Escape modifiers - Nix managed";
       rules = [
         {
           description = ''
@@ -55,8 +54,50 @@ in {
             }
           ];
         }
+        {
+          description = "Toggle caps_lock by pressing escape";
+          manipulators = [
+            {
+              type = "basic";
+              from = {
+                key_code = "escape";
+                modifiers = {optional = ["any"];};
+              };
+              to = [
+                {key_code = "caps_lock";}
+              ];
+            }
+          ];
+        }
       ];
     };
+
+    # "karabiner/assets/complex_modifications/nix.json".text = builtins.toJSON {
+    #   title = "CapsLock modifier - Nix managed";
+    #   rules = [
+    #     {
+    #       description = ''
+    #         Change caps_lock to left_control if pressed with other keys,
+    #         change caps_lock to escape if pressed alone.
+    #       '';
+    #       manipulators = [
+    #         {
+    #           type = "basic";
+    #           from = {
+    #             key_code = "caps_lock";
+    #             modifiers = {optional = ["any"];};
+    #           };
+    #           to = [
+    #             {key_code = "left_control";}
+    #           ];
+    #           to_if_alone = [
+    #             {key_code = "escape";}
+    #           ];
+    #         }
+    #       ];
+    #     }
+    #   ];
+    # };
 
     "raycast/latest.rayconfig" = {
       source = ./raycast/latest.rayconfig;
