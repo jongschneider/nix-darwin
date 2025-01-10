e7cdbbb86c6ceefb3c9a2f6008d2f8b1e734b294
Jonathan Schneider <jonathan.schneider@thetalake.com>
2025-01-10 08:58:37 -0500

    adds tmp.json to gitignore

--- home/default.nix
+++ home/default.nix
@@ -187,6 +187,7 @@ in {
         ".direnv"
         ".envrc"
         "tmp.txt"
+        "tmp*.json"
         "testout.txt"
         "permitted_addresses.txt"
         "postscreen_access.txt"
@@ -310,8 +311,8 @@ in {
 
         # Work
         localdev = "/Users/jschneider/Developer/localdev/localdev";
-        runingester = "export INGESTER_ENV=.env && goi && ingester > tmp.txt";
-        runintegrator = "export INTEGRATOR_ENV=.env && goi && integrator  > tmp.txt";
+        runingester = "export INGESTER_ENV=.env && goi && ingester > tmp.json";
+        runintegrator = "export INTEGRATOR_ENV=.env && goi && integrator  > tmp.json";
         runemailpreprocessor = "ENV_FILE=preprocessor.env.localdev go run ./cmd/email_preprocessor/...";
         telcon = "tsh login --proxy=thetalake.teleport.sh --auth=google";
         colcon = "echo 'creating an ssh tunnel to dev collector via teleport...\ntsh ssh -N -L 27777:localhost.thetalake.com:13579 ubuntu@ingester1.dev1.thetalake.com' && tsh ssh -N -L 27777:localhost.thetalake.com:13579 ubuntu@ingester1.dev1.thetalake.com";
