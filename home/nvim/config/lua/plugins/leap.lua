3c8c86a0670cced7f12b5827cbd13af635acab68
jonathan-schneider-tl <jonathan.schneider@thetalake.com>
2026-02-01 16:45:25 -0500

    fix(nvim): leap

--- home/nvim/config/lua/plugins/leap.lua
+++ home/nvim/config/lua/plugins/leap.lua
@@ -1,11 +1,6 @@
 return {
 	"ggandor/leap.nvim",
 	config = function()
-		-- Default mappings are now set automatically by leap.
-		-- Configure or override here only if needed.
-		require("leap").setup({})
-		-- Example custom remaps (commented out):
-		-- local leap = require("leap")
-		-- vim.keymap.set({"n","x","o"}, "s", leap.leap, { desc = "Leap forward" })
+		require("leap").create_default_mappings()
 	end,
 }
