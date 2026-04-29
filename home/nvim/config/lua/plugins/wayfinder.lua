312b01d8c44a00deae7d2dd90ae7c34330f07324
jongschneider <jongschneider@gmail.com>
2026-04-29 09:11:17 -0400

    feat(nvim): add wayfinder.nvim plugin

---
+++ home/nvim/config/lua/plugins/wayfinder.lua
@@ -0,0 +1,11 @@
+return {
+	"error311/wayfinder.nvim",
+	keys = {
+		{ "<leader>wf", "<Plug>(WayfinderOpen)", desc = "Wayfinder" },
+		{ "<leader>wtn", "<Plug>(WayfinderTrailNext)", desc = "Wayfinder Trail Next" },
+		{ "<leader>wtp", "<Plug>(WayfinderTrailPrev)", desc = "Wayfinder Trail Prev" },
+		{ "<leader>wto", "<Plug>(WayfinderTrailOpen)", desc = "Wayfinder Trail Open" },
+		{ "<leader>wts", "<Plug>(WayfinderTrailShow)", desc = "Wayfinder Trail Show" },
+	},
+	opts = {},
+}
